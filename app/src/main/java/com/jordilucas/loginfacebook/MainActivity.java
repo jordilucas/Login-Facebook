cae73991f6b13782834d758106d25513aae61a5b
Jordi Lucas <jordi@joyjet.com>
2017-11-27 11:42:41 -0300

    Show user data profile

--- app/src/main/java/com/jordilucas/loginfacebook/MainActivity.java
+++ app/src/main/java/com/jordilucas/loginfacebook/MainActivity.java
@@ -1,13 +1,86 @@
 package com.jordilucas.loginfacebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    ProfileTracker profileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile");
+        callbackManager = CallbackManager.Factory.create();
+
+        obterDadosFacebook();
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void obterDadosFacebook(){
+
+        List permissionsNeeds = Arrays.asList("email", "public_profile", "user_mobile_phone", "user_address");
+        LoginManager.getInstance().logInWithReadPermissions(this, permissionsNeeds);
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.v("TAG", "JSON: " + object);
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,picture.width(120).height(120)");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.e("TAG", "facebook login canceled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("TAG", "facebook login failed error");
+            }
+        });
+
+
+    }
+
 }
