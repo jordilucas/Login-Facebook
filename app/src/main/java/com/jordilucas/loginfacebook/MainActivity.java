307da3bee7f93f0ef796cea5a41fd793358fd86f
Jordi Lucas <jordi@joyjet.com>
2017-11-27 17:14:00 -0300

    Show foto and name user

--- app/src/main/java/com/jordilucas/loginfacebook/MainActivity.java
+++ app/src/main/java/com/jordilucas/loginfacebook/MainActivity.java
@@ -1,9 +1,14 @@
 package com.jordilucas.loginfacebook;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -16,9 +21,13 @@ import com.facebook.ProfileTracker;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     LoginButton loginButton;
     CallbackManager callbackManager;
     ProfileTracker profileTracker;
+    ImageView imageView;
+    TextView textView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +46,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loginButton = findViewById(R.id.login_button);
+        imageView = findViewById(R.id.profile_image);
+        textView = findViewById(R.id.name);
         loginButton.setReadPermissions("public_profile");
         callbackManager = CallbackManager.Factory.create();
+        progressBar = findViewById(R.id.progress_bar);
 
         obterDadosFacebook();
 
@@ -47,9 +62,17 @@ public class MainActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+
+
+
+    }
+
     private void obterDadosFacebook(){
 
-        List permissionsNeeds = Arrays.asList("email", "public_profile", "user_mobile_phone", "user_address");
+        List permissionsNeeds = Arrays.asList("email", "public_profile");
         LoginManager.getInstance().logInWithReadPermissions(this, permissionsNeeds);
 
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -59,6 +82,20 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
                         Log.v("TAG", "JSON: " + object);
+                        try {
+                            if(AccessToken.getCurrentAccessToken() == null){
+                                textView.setText("");
+                                imageView.invalidate();
+                            }
+
+                            textView.setText(object.getString("name"));
+                            recuperaFotoPerfilFacebook(object.getString("id"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
                     }
                 });
 
@@ -79,6 +116,26 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("TAG", "facebook login failed error");
             }
         });
+
+
+    }
+
+    private void recuperaFotoPerfilFacebook(String userID) throws MalformedURLException {
+        Uri.Builder builder = Uri.parse("https://graph.facebook.com").buildUpon();
+        builder.appendPath(userID).appendPath("picture").appendQueryParameter("type", "large");
+        Picasso.with(MainActivity.this).load(builder.toString()).into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
+
+
 
 
     }
